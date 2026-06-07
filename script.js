1fce1ef3dcd4d9e4be3c92acffd47e30faba5c93
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-06-07 12:33:03 -0700

    Create five rows for buttons and append to buttons container

--- script.js
+++ script.js
@@ -44,5 +44,27 @@ numberOnScreen.textContent = "21";
 
 let buttonsContainer = document.createElement("div");
 
+let row1 = document.createElement("div");
+row1.className = "row-1";
+
+let row2 = document.createElement("div");
+row2.className = "row-2";
+
+let row3 = document.createElement("div");
+row3.className = "row-3";
+
+let row4 = document.createElement("div");
+row4.className = "row-4";
+
+let row5 = document.createElement("div");
+row5.className = "row-5";
+
+
 calculatorDiv.appendChild(numberOnScreen);
 calculatorDiv.appendChild(buttonsContainer);
+
+buttonsContainer.appendChild(row1);
+buttonsContainer.appendChild(row2);
+buttonsContainer.appendChild(row3);
+buttonsContainer.appendChild(row4);
+buttonsContainer.appendChild(row5);
